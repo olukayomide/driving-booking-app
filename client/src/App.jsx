af7c0c28117141ff0d4cce8ae4b083b6a44d04e5
olukayomide <olukayomide@gmail.com>
2026-04-30 19:43:48 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+   {t}
+          </div>
+        ))}
+      </div>
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Driving Booking App</h1>
+      <h2 className="mt-6 font-bold">My Bookings</h2>
+
+      {bookings.map(b => (
+        <div key={b._id} className="border p-2 mt-2">
+          {b.name} | {b.date} | {b.startTime} ({b.duration}h)
+          <button
+            onClick={() => cancel(b._id)}
+            className="ml-2 bg-red-500 text-white px-2"
+          >
+            Cancel
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
-
-export default App;
